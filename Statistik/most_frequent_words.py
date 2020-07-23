7245f039df4cfc25abb42ecc9c55040133436705
piasu <pia.suelzle@gmx.de>
2020-07-23 17:30:14 +0200

    Update most_frequent_words.py

--- Statistik/most_frequent_words.py
+++ Statistik/most_frequent_words.py
@@ -2,33 +2,40 @@ from collections import Counter
 from spacy.lang.de.stop_words import STOP_WORDS
 import spacy
 import re
+import json
 
-dic = {
-            "titel": "Lachen und Schweigen",
-            "interpret": "Puhdys",
-            "platzierung": 4,
-            "lyrics": "Trug der Wind ihr Lachen durch die Nacht und sie tanzte in seinem Blick\nTrug der Wind ihr Lachen durch die Nacht und er nahm sie mit sich mit.\nDoch eine Nacht ist schnell vorbei, eine Nacht ist schnell vorbei.\nWenn ein Traum, wenn ein Traum, wenn ein Traum beginnt, spielen Perlen im Licht.\nWenn ein Traum, wenn ein Traum, wenn ein Traum beginnt, sieht man vieles nicht.\n\nTrug der Wind Musik durch diese Nacht und sein Zimmer war viel zu heiß.\nTrug der Wind Musik durch diese Nacht und sie kannte nicht seinen Preis.\nDenn eine Nacht ist schnell vorbei, eine Nacht ist schnell vorbei.\nWenn ein Traum, wenn ein Traum, wenn ein Traum beginnt, spielen Perlen im Licht.\nWenn ein Traum, wenn ein Traum, wenn ein Traum beginnt, sieht man vieles nicht.\n\nTrug der Wind ihr Schweigen durch die Nacht und die Straßen waren so kalt.\nTrug der Wind ihr Schweigen durch die Nacht eine Nacht, die sie längst bereut.\nDenn eine Nacht ist schnell vorbei, eine Nacht ist schnell vorbei.\nWenn ein Traum, wenn ein Traum, wenn ein Traum beginnt, spielen Perlen im Licht.\nWenn ein Traum, wenn ein Traum, wenn ein Traum beginnt, sieht man vieles nicht.\nWenn ein Traum, wenn ein Traum, wenn ein Traum beginnt, spielen Perlen im Licht.\nWenn ein Traum, wenn ein Traum, wenn ein Traum beginnt, sieht man vieles nicht.\n            ",
-            "tokenanzahl": 229,
-            "typeanzahl": 54,
-            "type/token": 23.58
-        }
+with open('/Users/pia/Desktop/Uni/Bachelor-Arbeit/DDR-BRD-comparison/Datenbeschaffung/Data/ddr_charts.json') as file:
+    ddr_hits = json.load(file)
+
+with open('/Users/pia/Desktop/Uni/Bachelor-Arbeit/DDR-BRD-comparison/Datenbeschaffung/Data/brd_charts.json') as file:
+    brd_hits = json.load(file)
+
 
 nlp = spacy.load('de')
-spacy_doc = nlp(dic["lyrics"])
 
-token = [tok.text.lower() for tok in spacy_doc]
-# Löschen der new lines
-for t in token:
-    if re.match(r'(\n)+\s*', t):
-        token.remove(t)
-# Löschen der Sonderzeichen
-for t in token:
-    if re.match(r'\'|\.\.\.|\.|´|!|\?|,|:|-|;', t):
-        token.remove(t)
-    
-removed_stopwords = [tok for tok in token if tok not in STOP_WORDS]
+for jahr in ddr_hits:
+    mfw_ddr = []
+    all_lyrics_ddr = ""
+    for i in ddr_hits[jahr]:
+        all_lyrics_ddr = all_lyrics_ddr + "\n\n" + i["lyrics"]
 
-Counter = Counter(removed_stopwords)
-most_occur = Counter.most_common(4) 
+    spacy_doc = nlp(all_lyrics_ddr)
 
-print(most_occur)
+    token = [tok.lemma_.lower() for tok in spacy_doc]
+    # Löschen der new lines
+    for t in token:
+        if re.match(r'(\n)+\s*', t):
+            token.remove(t)
+    # Löschen der Sonderzeichen
+    for t in token:
+        if re.match(r'\'|\.\.\.|\.|´|!|\?|,|:|–|;|`', t):
+            token.remove(t)
+        
+    token = [tok for tok in token if tok not in STOP_WORDS]
+
+    Counterv = Counter(token)
+    most_occur = Counterv.most_common(4) 
+
+    print(jahr + " waren die most frequent words:")
+    print(most_occur)
+    print("\n")
