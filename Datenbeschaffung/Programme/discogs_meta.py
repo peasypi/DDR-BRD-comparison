d54e5d406cf18a2ef7e8d6034b08269751acba38
piasu <pia.suelzle@gmx.de>
2020-11-13 17:25:41 +0100

    Update discogs_meta

--- Datenbeschaffung/Programme/discogs_meta.py
+++ Datenbeschaffung/Programme/discogs_meta.py
@@ -1,6 +1,7 @@
 import discogs_key as dk
 import requests
 import json
+import time
 
 DDR_PATH = '/Users/pia/Desktop/Uni/Bachelor-Arbeit/DDR-BRD-comparison/Datenbeschaffung/Data/ddr_charts.json'
 BRD_PATH = '/Users/pia/Desktop/Uni/Bachelor-Arbeit/DDR-BRD-comparison/Datenbeschaffung/Data/brd_charts.json'
@@ -11,38 +12,41 @@ paths = [DDR_PATH, BRD_PATH]
 def get_meta(jahr, titel, artist):
     key = dk.consumer_key
     secret = dk.consumer_secret
-    r = requests.get(f'https://api.discogs.com/database/search?track={titel}&artist={artist}&key={key}&secret={secret}')
+    song = titel + ' ' + artist
+    r = requests.get(f'https://api.discogs.com/database/search?q={song}&key={key}&secret={secret}')
     # print(r.json())
     page = r.json()
     # print(page['results'])
-    results = page['results']
-    if not results:
-        return 'Nothing'
-    else:
-        for re in results:
+    if 'results' in page:
+        results = page['results']
+        for re in results[:]:
+            if 'year' not in re.keys():
+                results.remove(re)
+        results_sorted = sorted(results, key=lambda k: k['year'])
+        for re in results_sorted:
+            #print(re, "\n\n\n")
             if int(re['year']) <= 1990:
                 #print(re, "\n")
                 genre = re['genre']
                 style = re['style']
                 label = re['label']
                 country = re['country']
-                break
-            else:
-                return 'Nothing'
-        return (genre, style, label, country)
-
+                return (genre, style, label, country)
 
 for p in paths:
     with open(p) as file:
         hit_json = json.load(file)
+        nicht_gefunden = []
 
         for jahr in hit_json:
             for song in hit_json[jahr]:
                 titel = song['titel']
                 artist = song['interpret']
                 data = get_meta(jahr, titel, artist)
-                if data == "Nothing":
+                time.sleep(2)
+                if data == None:
                     print(f'[ERROR]     Keine Metadaten zu {titel} von {artist} gefunden')
+                    nicht_gefunden.append(titel)
                 else:
                     genre, style, label, country = data[0], data[1], data[2], data[3]
                     song['label'] = label
@@ -53,5 +57,7 @@ for p in paths:
 
     with open(p, 'w') as up_file:
         json.dump(hit_json, up_file, indent=4, ensure_ascii=False)
+    
+    print(nicht_gefunden)
+    print(len(nicht_gefunden))
 
-# /database/search?q={query}&{?type,title,release_title,credit,artist,anv,label,genre,style,country,year,format,catno,barcode,track,submitter,contributor}
